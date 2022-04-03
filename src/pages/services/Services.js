9e48311b6654b07a3ec940d995b8890b2fd1eda3
phucleit <phucleit95@gmail.com>
2022-04-04 00:21:05 +0700

    update version react, react dom, them @mui, data table services

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -3,31 +3,86 @@ import {
   Button
 } from "@material-ui/core";
 import axios from 'axios';
+import { DataGrid } from '@mui/x-data-grid';
+import { DeleteOutline, Add } from '@mui/icons-material';
+import {
+  Link,
+} from "react-router-dom";
 
 // components
 import PageTitle from "../../components/PageTitle/PageTitle";
+import useStyles from "./styles";
 
 // icons sets
 import "font-awesome/css/font-awesome.min.css";
 
-export default function ServicesPage () {
+const url = `https://backend.omcloud.vn/api/service/`;
 
-  const handleClick = (e) => {
-    
+export default function ServicesPage () {
+  var classes = useStyles();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadServices();
+  }, []);
+
+  const loadServices = async () => {
+    const result = await axios.get('https://backend.omcloud.vn/api/service');
+    setData(result.data.data);
+  };
+
+  const handleDelete = (id) => {
+
   }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Tên dịch vụ', width: 250 },
+    { 
+      field: 'service_type', 
+      headerName: 'Loại dịch vụ', 
+      width: 250, 
+      valueGetter: (params) => `${params.row.service_type.name}` 
+    },
+    { field: 'construction_count', headerName: 'Số lượng công trình', width: 200 },
+    { 
+      field: 'hanhDong', 
+      headerName: 'Hành động', 
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div className={classes.buttonAction}>
+            <Link to={"/app/services/"+params.row.id}>
+              <button className={classes.serviceListEdit}>Edit</button>
+            </Link>
+            <DeleteOutline className={classes.serviceListDelete} onClick={() => handleDelete(params.row.id)} />
+          </div>
+        );
+      }
+    },
+  ];
 
   return (
     <>
       <PageTitle title="Danh sách dịch vụ" button={(
-        <Button
-          variant="contained"
-          size="medium"
-          color="secondary"
-          onClick={e => handleClick(e)}
-        >
-          Thêm mới
-        </Button>
+        <Link to="/app/new-service">
+          <Button
+            variant="contained"
+            size="medium"
+            color="secondary"
+          >
+            Thêm mới
+          </Button>
+        </Link>
       )} />
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+        className={classes.serviceData}
+      />
     </>
   );
 }
--- src/pages/services/styles.js
+++ src/pages/services/styles.js
@@ -29,4 +29,24 @@ export default makeStyles(theme => ({
     overflow: "hidden",
     paddingBottom: theme.spacing(2),
   },
+  serviceData: {
+    background: '#fff',
+  },
+  serviceListEdit: {
+    border: 'none',
+    borderRadius: '10px',
+    padding: '5px 10px',
+    backgroundColor: '#3bb077',
+    color: '#fff',
+    cursor: 'pointer',
+    marginRight: '10px',
+  },
+  serviceListDelete: {
+    color: '#f00',
+    cursor: 'pointer',
+  },
+  buttonAction: {
+    display: 'flex',
+    alignItems: 'center',
+  }
 }));
